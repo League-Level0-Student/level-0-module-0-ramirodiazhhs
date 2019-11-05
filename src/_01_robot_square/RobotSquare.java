d95786067f4f4433ef70a0cf28dd9b04ce5262b9
366686 <366686@0338S-S-R0B6H0L.sdcs.local>
2019-11-05 14:24:41 -0800

    robotgraffiti

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -12,22 +12,25 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+        Robot r2d2 = new Robot();
+        
 
         // 3. Put the robot's pen down
-
+         r2d2.penDown();
 
         // 6. Make the robot move as fast as possible
-
+          r2d2.setSpeed(100);
 
         // 5. Do everything below here 4 times
-
+            for (int i = 0; i < 4 ; i++) {
+				
+			
 
         //         2. Move your robot 200 pixels
-
+        	   r2d2.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
+                    r2d2.turn(90);
 
     }
-}
+}}
